2cfc294bfa7ec5a10aa39f5eca32c1ea7477d661
Stephen Chapman <Schapm16@gmail.com>
2018-05-04 13:57:45 -0400

    Completed the 'Routing' section of the tutorial

--- Heroes/src/app/app.module.ts
+++ Heroes/src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+import { HeroService } from './hero.service';
+
+
+
 
 @NgModule({
   imports: [ 
   	BrowserModule,
-  	FormsModule
-  	],
-  declarations: [ 
-  	AppComponent, 
-  	HeroDetailComponent
+  	FormsModule,
+    AppRoutingModule
   ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ 
+  	AppComponent,
+    DashboardComponent, 
+  	HeroDetailComponent,
+  	HeroesComponent,
+  ],
+  bootstrap: [ AppComponent ],
+  providers: [ HeroService ]
 })
 export class AppModule { }
